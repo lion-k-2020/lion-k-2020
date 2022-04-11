441ebd748d68e6615c25339fa672e75dad173e70
lion <1678959024@qq.com>
2022-04-12 02:34:00 +0800

    Update views.py

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -4,7 +4,7 @@ from run import app
 from wxcloudrun.dao import delete_counterbyid, query_counterbyid, insert_counter, update_counterbyid, delete_articlebyid, query_articlebyid, insert_article, update_articlebyid
 from wxcloudrun.model import Counters, Article
 from wxcloudrun.response import make_succ_empty_response, make_succ_response, make_err_response
-from wxcloudrun.sys import uuid
+from wxcloudrun.sys import suid
 
 
 @app.route('/')
