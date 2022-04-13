f5668c0c6a25eb9030a865a0559d3bb1513199c3
lion <1678959024@qq.com>
2022-04-13 17:09:50 +0800

    Update model.py

--- wxcloudrun/model.py
+++ wxcloudrun/model.py
@@ -23,5 +23,45 @@ class Article(db.Model):
     update_time = db.Column(db.TIMESTAMP, onupdate=datetime.now(), default=datetime.now())
     create_time = db.Column(db.TIMESTAMP, default=datetime.now())
 
+class Tab(db.Model):
+    __tablename__ = 'tab'
+    id = db.Column(db.String(32), primary_key=True, autoincrement=False)
+    name = db.Column(db.String(50), nullable=False, name='标签名称')
+    index = db.Column(db.Integer, unique=True, autoincrement=True)
+    create_time = db.Column(db.TIMESTAMP, default=datetime.now())
+    update_time = db.Column(db.TIMESTAMP, default=datetime.now())
+    deleted = db.Column(db.Integer(2), default=0)
 
+class Video(db.Model):
+    __tablename__ = 'video'
+    id = db.Column(db.String(32), primary_key=True, autoincrement=False)
+    tab_id = db.Column(db.String(32), nullable=False)
+    name = db.Column(db.String(50), nullable=False, name='视频名称')
+    cover_src = db.Column(db.String(100), nullable=False)
+    src = db.Column(db.String(100), nullable=False)
+    index = db.Column(db.Integer, unique=True, autoincrement=True)
+    create_time = db.Column(db.TIMESTAMP, default=datetime.now())
+    update_time = db.Column(db.TIMESTAMP, default=datetime.now())
+    deleted = db.Column(db.Integer(2), default=0)
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.String(50), primary_key=True, autoincrement=False)
+    user_id = db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String(50), nullable=False, name='用户名称')
+    is_vip = db.Column(db.Integer(2), default=0)
+    create_time = db.Column(db.TIMESTAMP, default=datetime.now())
+    update_time = db.Column(db.TIMESTAMP, default=datetime.now())
+    deleted = db.Column(db.Integer(2), default=0)
+
+class Favorite(db.Model):
+    __tablename__ = 'favorite'
+    id = db.Column(db.String(32), primary_key=True, autoincrement=False)
+    user_id = db.Column(db.String(32), nullable=False)
+    video_id = db.Column(db.String(32), nullable=False)
+    is_favorite = db.Column(db.Integer(2), default=0)
+    create_time = db.Column(db.TIMESTAMP, default=datetime.now())
+    update_time = db.Column(db.TIMESTAMP, default=datetime.now())
+    deleted = db.Column(db.Integer(2), default=0)
+	
 db.create_all()
