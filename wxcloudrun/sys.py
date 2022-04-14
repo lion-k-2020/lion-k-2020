8e0cf855755fc007c55d5718e05d70e9e1b6527a
lion <1678959024@qq.com>
2022-04-14 20:04:24 +0800

    Update sys.py

--- wxcloudrun/sys.py
+++ wxcloudrun/sys.py
@@ -1,12 +1,7 @@
 import uuid
-import json
 
 def suid():
     uid = str(uuid.uuid4())
     suid = ''.join(uid.split('-'))
     return suid
 
-def MyEncoder(obj):
-    if isinstance(obj, bytes):
-        return str(obj, encoding='utf-8')
-    return json.JSONEncoder.default(obj)
