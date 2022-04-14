cc3219f0a85f9698a909f9990e73d649e6d817a1
lion <1678959024@qq.com>
2022-04-15 03:28:47 +0800

    Update myEncoder.py

--- wxcloudrun/myEncoder.py
+++ wxcloudrun/myEncoder.py
@@ -8,14 +8,34 @@ class AlchemyEncoder(json.JSONEncoder):
     """
  
     def default(self, obj):
-        if isinstance(obj.__class__, DeclarativeMeta):
-            fields = {}
-            for field in [x for x in dir(obj) if not x.startswith('_') and x != 'metadata']:
-                data = obj.__getattribute__(field)
-                try:
-                    json.dumps(data)
-                    fields[field] = data
-                except TypeError:
-                    fields[field] = None
-            return fields
-        return json.JSONEncoder.default(self, obj)
+		if isinstance(obj, Query):
+			fileds = []
+			record = {}
+			for rec in obj.all():
+				for field in [x for in dir(rec) if
+					not x.startswith('_')
+					and hasattr(rec._getattribute_(x), '_call_') == False
+					and x != 'metadata'];
+				data = rec._getattribute_(field)
+				try:
+					record[field] = data
+				except TypeError:
+					record[field] = None
+			fields.append(record)
+			return fields
+		return json.JSONEncoder.default(self, obj)
+	
+	
+	
+	
+#         if isinstance(obj.__class__, DeclarativeMeta):
+#             fields = {}
+#             for field in [x for x in dir(obj) if not x.startswith('_') and x != 'metadata']:
+#                 data = obj.__getattribute__(field)
+#                 try:
+#                     json.dumps(data)
+#                     fields[field] = data
+#                 except TypeError:
+#                     fields[field] = None
+#             return fields
+#         return json.JSONEncoder.default(self, obj)
