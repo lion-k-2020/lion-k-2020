289bc50a32b7f804e54bf12d0b568177553e2ad9
lion <1678959024@qq.com>
2022-04-15 14:03:18 +0800

    Update model.py

--- wxcloudrun/model.py
+++ wxcloudrun/model.py
@@ -86,7 +86,7 @@ def get_data():
     users_output = []
     for tab in tabs:
         users_output.append(tab.to_json())
-    data = json.dumps({'code': 0, 'data': data}, ensure_ascii=False)
+    data = json.dumps({'code': 0, 'data': users_output}, ensure_ascii=False)
     return Response(data, mimetype='application/json')
 	
 	
