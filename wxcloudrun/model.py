87410e77f85ab63a5b77c5330e66cebc82ef8b50
lion <1678959024@qq.com>
2022-04-15 15:36:31 +0800

    Update model.py

--- wxcloudrun/model.py
+++ wxcloudrun/model.py
@@ -82,11 +82,11 @@ def get_data():
     """
     :return: 小程序的tabs和videos
     """
-    tabs = db.session.query(Tab.id, Tab.index).filter(Tab.deleted == 0).all()
+    tabs = db.session.query(Tab.id, Tab.name, Tab.index).filter(Tab.deleted == 0).all()
     users_output = []
 #     for tab in tabs:
 #         users_output.append(tab.to_json())
-    datas = {'name': tabs[0]['name'], 'id': tabs[1]['id']}
+    datas = {'name': tabs[0]['name'], 'id': tabs[1]['id'], 'count', len(tabs)}
     data = json.dumps({'code': 0, 'data': datas}, ensure_ascii=False)
 #     data = json.dumps({'code': 0, 'data': users_output}, ensure_ascii=False)
     return Response(data, mimetype='application/json')
