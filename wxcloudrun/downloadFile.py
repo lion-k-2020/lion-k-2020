9d64557d1b9721dee1329b16bbc697e25043ce18
lion <1678959024@qq.com>
2022-04-17 01:36:24 +0800

    Update downloadFile.py

--- wxcloudrun/downloadFile.py
+++ wxcloudrun/downloadFile.py
@@ -1,11 +1,10 @@
 from urllib import request
 import requests
 
-def download_file(open_id, url):
-    url = 'https://7072-prod-4gbd4dc41c3a1678-1310533907.tcb.qcloud.la/92ec59b3ec1bf80ccc2d805ef5f6bcdb.mp4'
+def download_file(open_id, url, mode):
     r = requests.get(url, allow_redirects=True)
     header = r.headers.get('content-type')
     type = header.split('/')[1]
-    save_path = "../upload_files" + open_id + "." + type
+    save_path = "../upload_files" + open_id + "_" + mode + "." + type
     request.urlretrieve(url, save_path)
     return save_path
