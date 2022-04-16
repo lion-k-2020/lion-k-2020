1255887ae85341db29ff2614b7f9b96ce54c2e81
lion <1678959024@qq.com>
2022-04-17 01:40:06 +0800

    Update downloadFile.py

--- wxcloudrun/downloadFile.py
+++ wxcloudrun/downloadFile.py
@@ -1,10 +1,10 @@
 from urllib import request
 import requests
 
-def download_file(open_id, url, mode):
+def download_file(openid, url, mode):
     r = requests.get(url, allow_redirects=True)
     header = r.headers.get('content-type')
     type = header.split('/')[1]
-    save_path = "../upload_files" + open_id + "_" + mode + "." + type
+    save_path = "../upload_files" + openid + "_" + mode + "." + type
     request.urlretrieve(url, save_path)
     return save_path
