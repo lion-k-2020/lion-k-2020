a6d44a862bda7f5c12d2e4d8e5e25889b8892450
lion <1678959024@qq.com>
2022-04-17 13:09:52 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(_name_)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 # 是否开启debug模式
 DEBUG = True
